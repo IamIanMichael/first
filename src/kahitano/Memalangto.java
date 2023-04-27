c43426b68c65e8106c45d2518382612d76d2e574
IamIanMichael <ianmichaelcatindoy@gmail.com>
2023-04-28 01:56:28 +0800

    Inital commit

---
+++ src/kahitano/Memalangto.java
@@ -0,0 +1,10 @@
+package kahitano;
+
+public class Memalangto {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author TESTING
+ *
+ */
+module kahitano {
+}
